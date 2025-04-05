e6fee6082999ace5b80447bbb99cb4b77aa2424c
juyeon Bae <w2316@e-mirim.hs.kr>
2025-04-06 02:24:25 +0900

    feat : 테스트 선택하는 화면 구현

---
+++ components/Practice/Card.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, Image, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+
+
+export default function ({ subject, title, source }) {
+    const navigation = useNavigation();
+    const handlePress = () => {
+        if (subject === "English Practice") {
+            navigation.navigate('SelectTest')
+        }
+        if (subject === "Australia Culture Quiz") {
+            navigation.navigate('TranslationTest')
+        }
+    }
+    return (
+        <TouchableOpacity onPress={handlePress}
+            style={{
+                backgroundColor: "white", marginBottom: 16,
+                width: 335, height: 120, borderRadius: 16,
+                shadowColor: "#000",
+                shadowOffset: { width: 0, height: 1 },
+                shadowOpacity: 0.1,
+                shadowRadius: 4,
+                elevation: 2,
+            }}>
+            <View style={{ flexDirection: "row", paddingLeft: 20, paddingVertical: 37 }}>
+                <Image
+                    source={source}
+                    style={{ width: 60, height: 60, marginTop: -10 }}
+                />
+
+                <View style={{ marginLeft: 13 }}>
+                    <Text
+                        style={{
+                            fontSize: 20, fontWeight: '600', fontFamily: "Pretendard-Regular",
+                        }}>{subject}</Text>
+                    <Text style={{ fontSize: 12, color: "#777", marginTop: 8, fontFamily: "Pretendard-Regular" }}>{title}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    );
+
+
+}
