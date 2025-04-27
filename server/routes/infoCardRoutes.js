5bd474169cc33a65757c191fa453767f7ab2d6eb
juyeonBae <w2316@e-mirim.hs.kr>
2025-04-27 23:09:50 +0900

    feat : 모든 InfoCard 데이터 조회하는 api (test)

---
+++ server/routes/infoCardRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const InfoCard = require('../models/InfoCard')
+
+//모든 infocard 데이터 조회
+router.get('/', async (req, res) => {
+    try {
+        const cards = await InfoCard.find()
+    } catch (err) {
+        res.status(500).json({ message: 'server error' })
+    }
+})
+
