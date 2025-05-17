b4172f9ddb36caf2cfc1566878c7493c54274327
juyeonBae <w2316@e-mirim.hs.kr>
2025-05-17 13:50:53 +0900

    feat : 카테고리 버튼 컴포넌트 구현

---
+++ components/Home/Category.tsx
@@ -0,0 +1,36 @@
+import { View, Text, StyleSheet } from "react-native"
+import React from "react"
+
+type CategoryProps = {
+    type: string;
+    onPress: () => void;
+    isActive: boolean
+
+}
+export default function Category({ type, onPress, isActive }: CategoryProps) {
+    return (
+        <View style={[styles.Container, isActive && styles.activeBg]}>
+            <Text
+                onPress={onPress}
+                style={[styles.category, isActive && styles.activeCategory]}
+            >{type}</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    activeBg: { backgroundColor: "#FFF9C4" },
+    Container: {
+        backgroundColor: "#FFF", borderRadius: 16, height: 24,
+        paddingHorizontal: 17, paddingVertical: 2,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 2,
+    },
+    category: {
+        textAlign: "center", fontSize: 12, color: "#777",
+        fontWeight: "400", fontFamily: 'Pretendard-bold'
+    },
+    activeCategory: { color: "#FFB600", fontWeight: "600" }
+})
