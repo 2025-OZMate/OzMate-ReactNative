f133d12a9c1fd6f6c841750fcf07eda944fe4239
juyeonBae <w2316@e-mirim.hs.kr>
2025-05-28 23:15:45 +0900

    fix : 환율 계산기 style 추가

--- screens/ExchangeRate/ExchangeRate.tsx
+++ screens/ExchangeRate/ExchangeRate.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-import { View, TextInput, Text, Button, Alert, StyleSheet } from "react-native";
+import { View, Text, Image, Alert, StyleSheet, TouchableOpacity } from "react-native";
 import RateButton from "../../components/common/RateButton";
-
+import PrevBtn from "../../components/common/PrevBtn";
+import CalculateCard from "../../components/common/CalculateCard";
+import { useNavigation } from "@react-navigation/native";
 export default function ExchangeRate() {
     const [krw, setKrw] = useState('');
     const [Aud, setAud] = useState(null);
@@ -20,34 +22,93 @@ export default function ExchangeRate() {
             alert('환율 정보를 불러올 수 없습니다.')
         }
     }
+
+    const navigation = useNavigation()
+
+    function Prev() {
+        return (
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                <Image source={require('../../assets/images/prev.png')}
+                    style={{ width: 38, height: 38, marginLeft: 15 }} />
+            </TouchableOpacity>
+        )
+    }
+
+    function ResetBtn() {
+        return (
+            <>
+                <TouchableOpacity onPress={() => { setAud(0); setKrw(''); }} style={styles.resetBtn}>
+                    <Text style={styles.resetText}>Reset</Text>
+                </TouchableOpacity >
+            </>
+        )
+    }
+
+    const formNum = (num: string) => {
+        const cleand = num.replace(/[^0-9]/g, '')
+        return cleand.replace(/\B(?=(\d{3})+(?!\d))/g, ',')//3자리마다 ,
+    }
+
     return (
         <View style={styles.krwInputContainer}>
-            <Text>환율 계산기(￦->aud)</Text>
-            <TextInput
-                style={styles.TextInput}
-                placeholder="krw 입력"
-                keyboardType="numeric" //ios에서 계산기 형태로 나오게 함
-                value={krw}
-                onChangeText={setKrw}
-            />
-            <View>
-                <RateButton
-                    onPress={handleExchangeRate}
-                    title="계산하기"
-                />
+            <View style={{ marginTop: 50, display: "flex", flexDirection: "row", alignItems: "center", gap: 13 }}>
+                <Prev />
+                <Text style={styles.mainText}>Exchange Rate Calculate</Text>
             </View>
 
+            <Text style={styles.description}>Enter Korean money and press Calculate!</Text>
 
-            {/*결과*/}
-            <View>
-                {Aud && <Text>계산한 호주 돈 : {Aud}</Text>}
+            <View style={{ paddingHorizontal: 20 }}>
+                <View style={styles.rateContainer}>
+                    <Text style={styles.showExchangeRateText}>{formNum(krw)} KRW = {Aud} AUD</Text>
+                    <ResetBtn />
+                </View>
+
+                <View style={styles.calculateCardContainer}>
+                    <CalculateCard
+                        moneyTitle={"Amount"}
+                        moneyValue={"KRD"}
+                        placeholder={"Enter Korean money"}
+                        num={1}
+                        krw={krw}
+                        setKrw={setKrw}
+                    />
+                    <View style={styles.line}></View>
+                    <CalculateCard
+                        moneyTitle={"Converted Amount"}
+                        moneyValue={"AUD"}
+                        placeholder={""}
+                        num={2}
+                        AudMoney={Aud}
+                    />
+                </View>
+                <RateButton
+                    onPress={handleExchangeRate}
+                    title="Calculate"
+                />
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    krwInputContainer: { height: 100, borderColor: "yellow" },
+    calculateCardContainer: {
+        paddingHorizontal: 15, paddingVertical: 25,
+        borderRadius: 24,
+        backgroundColor: '#FFF',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+    },
+    rateContainer: { display: "flex", flexDirection: "row", justifyContent: "space-between", marginTop: 30, marginBottom: 20 },
+    resetText: { fontFamily: "Pretendard-Regaular", fontSize: 12.5, color: "#FF9E00" },
+    resetBtn: { width: 56, borderWidth: 0.5, borderRadius: 10, borderColor: "#FFC32E", paddingHorizontal: 11, paddingBottom: 4 },
+    line: { backgroundColor: "#F0F0F0", width: 316, height: 1.5, marginVertical: 20 },
+    showExchangeRateText: { color: "#606060", fontSize: 15, fontWeight: "500" },
+    description: { marginTop: 40, paddingLeft: 20, fontFamily: "Pretendard-Regaular", fontSize: 13, color: "#888" },
+    mainText: { fontFamily: "Pretendard-Regaular", fontSize: 24, fontWeight: "600" },
+    krwInputContainer: { backgroundColor: "#FFF", flex: 1 },
     TextInput: {
         height: 100, borderWidth: 1, borderColor: "red",
         marginVertical: 15, paddingHorizontal: 15
