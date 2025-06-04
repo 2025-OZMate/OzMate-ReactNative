d5306cca80ea16dfab7f80acac74c535cd8e786d
juyeonBae <w2316@e-mirim.hs.kr>
2025-06-04 23:57:48 +0900

    fix : 영문자 대문자 처리

--- components/common/Time.tsx
+++ components/common/Time.tsx
@@ -11,10 +11,10 @@ export default function Time() {
         return dayjs().tz("Australia/Sydney").format("hh:mm a")
     }
     const getAusDay = () => {
-        return dayjs().tz("Australia/Sydney").format("MMMM D, YYYY (ddd)");
+        return dayjs().tz("Australia/Sydney").format("MMMM D, YYYY (ddd)").toUpperCase();
     }
     return (
-        <View style={{ margin: "auto" }}>
+        <View style={{ margin: "auto", paddingTop: 120 }}>
             <Text style={[styles.font, styles.ausTime]}>{getAusTime()}</Text>
             <Text style={[styles.font, styles.ausDay]}>{getAusDay()}</Text>
         </View>
@@ -22,6 +22,6 @@ export default function Time() {
 }
 const styles = StyleSheet.create({
     font: { fontFamily: "Pretendard-Regaular" },
-    ausTime: { fontSize: 36, color: "#1E1E1E", fontWeight: "500" },
-    ausDay: { fontSize: 18, fontWeight: "400", marginTop: 4 },
+    ausTime: { fontSize: 40, color: "#1E1E1E", fontWeight: "500" },
+    ausDay: { fontSize: 18, fontWeight: "400", marginTop: 4, color: "#3F3F3F" },
 })
