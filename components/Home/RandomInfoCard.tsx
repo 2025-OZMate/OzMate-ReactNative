6510005e33d3a552840833ddfb235acb4ea953b2
juyeonBae <w2316@e-mirim.hs.kr>
2025-06-14 14:46:41 +0900

    fix : 클릭 시 화면 맨 윗 상단으로 올라가도록 수정

--- components/Home/RandomInfoCard.tsx
+++ components/Home/RandomInfoCard.tsx
@@ -6,77 +6,76 @@ import { useEffect } from "react";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import Constants from 'expo-constants';
+import Constants from "expo-constants";
 
 type RootStackParamList = {
-    DetailInfo: { id: any }
-}
+  DetailInfo: { id: any };
+};
 
-type NavigationProp = StackNavigationProp<RootStackParamList>
+type NavigationProp = StackNavigationProp<RootStackParamList>;
 
 interface Card {
-    _id: string,
-    cardId: string,
-    image: string,
-    title: string,
-    subtitle: string,
-    category: string
+  _id: string;
+  cardId: string;
+  image: string;
+  title: string;
+  subtitle: string;
+  category: string;
 }
 
 export default function RandomInfoCard() {
-    const apiURL = Constants.expoConfig?.extra?.apiUrl ?? "";
-    const navigation = useNavigation<NavigationProp>()
-    const [randomCard, setRandomCard] = useState<Card[]>([])
+  const apiURL = Constants.expoConfig?.extra?.apiUrl ?? "";
+  const navigation = useNavigation<NavigationProp>();
+  const [randomCard, setRandomCard] = useState<Card[]>([]);
 
-    useEffect(() => {
-        const fetchRandomCard = async () => {
-            const cardId = await AsyncStorage.getItem("cardId")
-            try {
-                const res = await axios.get(`${apiURL}/random/${cardId}`)
-                setRandomCard(res.data)
-                console.log(res)
+  useEffect(() => {
+    const fetchRandomCard = async () => {
+      const cardId = await AsyncStorage.getItem("cardId");
+      try {
+        const res = await axios.get(`${apiURL}/random/${cardId}`);
+        setRandomCard(res.data);
+        console.log(res);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchRandomCard();
+  }, []);
 
-            } catch (err) {
-                console.error(err)
-            }
-        }
-        fetchRandomCard()
-    }, [])
+  return (
+    <View style={{ backgroundColor: "#FFF", marginBottom: 20 }}>
+      <View style={styles.contour}></View>
+      <Text style={styles.text}>Other information</Text>
 
-    return (
-        <View style={{ backgroundColor: "#FFF", marginBottom: 20 }}>
-            <View style={styles.contour}></View>
-            <Text style={styles.text}>Other information</Text>
-
-            {randomCard.map((item) => (
-                <InfoCard
-                    key={item._id}
-                    _id={item._id}
-                    image={item.image}
-                    title={item.title}
-                    subtitle={item.subtitle}
-                    category={item.category}
-                    onPress={() => {
-                        navigation.navigate('DetailInfo', { id: item._id });
-                    }}
-                />
-            ))}
-        </View>
-    )
+      {randomCard.map((item) => (
+        <InfoCard
+          key={item._id}
+          _id={item._id}
+          image={item.image}
+          title={item.title}
+          subtitle={item.subtitle}
+          category={item.category}
+          onPress={() => {
+            navigation.push("DetailInfo", { id: item._id });
+          }}
+        />
+      ))}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    text: {
-        fontFamily: 'Pretendard-bold',
-        fontWeight: "500",
-        fontSize: 24,
-        marginLeft: 20,
-        marginVertical: 20
-    },
-    contour: {
-        width: "100%",
-        height: 12,
-        backgroundColor: "#EEE",
-        marginTop: 20
-    }
-})
+  text: {
+    fontFamily: "Pretendard-bold",
+    fontWeight: "500",
+    fontSize: 24,
+    marginLeft: 20,
+    marginVertical: 20,
+  },
+  contour: {
+    width: "100%",
+    height: 12,
+    backgroundColor: "#EEE",
+    marginTop: 20,
+  },
+});
